b3538351ff1d33773a31ab7419dd9daf9a3662ae
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2023-02-22 16:57:27 +0900

    시험 성적 출력

---
+++ Bronze5/9498.py
@@ -0,0 +1,14 @@
+i = int(input())
+if 90 <= i:
+    print("A")
+elif 80 <= i < 90:
+    print("B")
+elif 70 <= i < 80:
+    print("C")
+elif 60 <= i < 70:
+    print("D")
+else:
+    print("F")
+    
+#숏코딩
+print("FFFFFFDCBAA"[int(input())//10])
