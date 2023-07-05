fca25629629346bf5cb431c4912afa98813d4457
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2023-07-05 20:11:36 +0900

    Update 1145.py

--- Bronze1/1145.py
+++ Bronze1/1145.py
@@ -1,16 +1,19 @@
+# 적어도 대부분의 배수
 
-num = [int(i) for i in input().split() ]
+num = [int(i) for i in input().split(" ")]
 num.sort()
 
+cnt = 0
 i = num[2]
-
 while True:
-    i+=1
-    cnt = 0
+    
     for j in range(5):
         if i % num[j] == 0:
             cnt += 1
     if cnt >= 3:
         break
-
+    
+    cnt = 0   
+    i += 1
+    
 print(i)
